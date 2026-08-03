a8a5eb3140c0d15ada0646792a1ac684033e2d34
sypnosis2019 <sypnosistechnologies@gmail.com>
2026-08-03 16:34:34 +0530

    Product list

---
+++ app/routes/app._index.tsx
@@ -0,0 +1,12 @@
+export default function Index() {
+  return (
+    <s-page heading="Shopify Learning App">
+      <s-section>
+        <s-heading>Home</s-heading>
+        <s-paragraph>Welcome to your learning app.</s-paragraph>
+        <s-link href="/app/products">View first 5 products</s-link>
+      </s-section>
+    </s-page>
+  );
+}
+
