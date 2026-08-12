f4b5afa2cf1ee44d058041424758a74df36c7a03
Sypnosis Technologies <48352951+sypnosis2019@users.noreply.github.com>
2026-08-12 16:10:41 +0530

    Clean global types for fulfillment app

--- app/globals.d.ts
+++ app/globals.d.ts
@@ -1,6 +1,4 @@
 declare module "*.css";
-<<<<<<< HEAD
-=======
 
 export {};
 
@@ -15,13 +13,6 @@ declare global {
       ["s-paragraph"]: any;
       ["s-button"]: any;
       ["s-banner"]: any;
-      ["s-text-field"]: any;
-      ["s-drop-zone"]: any;
-      ["s-unordered-list"]: any;
-      ["s-list-item"]: any;
-      ["s-select"]: any;
-      ["s-option"]: any;
     }
   }
 }
->>>>>>> 9e4c2c592b902ec0c84295770032716c78d31109
