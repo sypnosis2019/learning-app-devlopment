d3466833470257f16723c30a5adf7b2e83dc7ef8
Sypnosis Technologies <48352951+sypnosis2019@users.noreply.github.com>
2026-08-12 16:10:32 +0530

    Fix app navigation conflict markers

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -2,16 +2,10 @@ import type { HeadersFunction, LoaderFunctionArgs } from "react-router";
 import { Outlet, useLoaderData, useRouteError } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
-
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   await authenticate.admin(request);
-<<<<<<< HEAD
-
-  // eslint-disable-next-line no-undef
-=======
->>>>>>> 9e4c2c592b902ec0c84295770032716c78d31109
   return { apiKey: process.env.SHOPIFY_API_KEY || "" };
 };
 
@@ -21,23 +15,13 @@ export default function App() {
   return (
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
-<<<<<<< HEAD
-        <s-link href="/app">Home</s-link>
-        <s-link href="/app/products">Products</s-link>
-        <s-link href="/app/additional">Additional page</s-link>
-=======
         <s-link href="/app">Auto Fulfillment Routing</s-link>
->>>>>>> 9e4c2c592b902ec0c84295770032716c78d31109
       </s-app-nav>
       <Outlet />
     </AppProvider>
   );
 }
 
-<<<<<<< HEAD
-// Shopify needs React Router to catch some thrown responses, so that their headers are included in the response.
-=======
->>>>>>> 9e4c2c592b902ec0c84295770032716c78d31109
 export function ErrorBoundary() {
   return boundary.error(useRouteError());
 }
